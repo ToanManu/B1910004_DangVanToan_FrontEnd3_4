d6f01ef690ff80ffe4c0280993713433952c54bb
B1910004 Dang Van Toan <ng.vnton@yahoo.com.vn>
2023-11-02 14:48:07 +0700

    up

--- contactbook-frontend/src/router/index.js
+++ contactbook-frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
+
 const routes = [
     {
         path: "/",
@@ -16,10 +17,18 @@ const routes = [
         name: "contact.edit",
         component: () => import("@/views/ContactEdit.vue"),
         props: true // Truyền các biến trong $route.params vào làm props
-        },
+    },
+    {
+        path: "/add_contact",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+        props: true
+    }
 ];
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 });
+
 export default router;
